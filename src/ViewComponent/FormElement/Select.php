41a3c33d00f5eeca9e1e277ab09a10804986f131
Tilo Kowalski <privat@tilokowalski.de>
2022-11-24 13:32:19 +0100

    view components

--- src/ViewComponent/FormElement/Select.php
+++ src/ViewComponent/FormElement/Select.php
@@ -4,8 +4,19 @@ class Delight_ViewComponent_FormElement_Select extends Delight_ViewComponent_For
 
     private $options;
 
-    public function add_option($key, $value) {
-        $this->options[$key] = $value;
+    private $prefilled_value;
+
+    public function add_option($value, $title) {
+        $this->options[$value] = $title;
+    }
+
+    public function set_prefilled_value($prefilled_value = null) { 
+        $this->prefilled_value = $prefilled_value;
+        return $this;
+    }
+
+    public function get_prefilled_value() {
+        return $this->prefilled_value;
     }
 
 }
