b374d3aba353db899b6ede6efe23ffe008c9bae9
Tilo Kowalski <privat@tilokowalski.de>
2023-02-04 20:23:12 +0100

    tinymce plugins & toolbar editable

--- src/ViewComponent/FormElement/TinyMCE.php
+++ src/ViewComponent/FormElement/TinyMCE.php
@@ -6,4 +6,29 @@ use Delight\ViewComponent\FormElement\TextArea;
 
 class TinyMCE extends TextArea {
 
+    const DEFAULT_PLUGINS = 'fullscreen table charmap lists wordcount';
+    const DEFAULT_TOOLBAR = 'undo redo | formatselect | bold italic underline | fontsize | alignleft aligncenter alignright alignjustify | numlist bullist | outdent indent | fullscreen removeformat';
+
+    private string $plugins;
+    private string $toolbar;
+
+    public function set_plugins(string $plugins): void {
+        $this->plugins = $plugins;
+    }
+
+    public function set_toolbar(string $toolbar): void {
+        $this->toolbar = $toolbar;
+    }
+
+    public function get_plugins(): string {
+        if ($this->plugins == null) return self::DEFAULT_PLUGINS;
+        return $this->plugins;
+    }
+
+    public function get_toolbar(): string {
+        if ($this->toolbar == null) return self::DEFAULT_TOOLBAR;
+        return $this->toolbar;
+    }
+
+
 }
