1b616c6bf6f8e9ff095d63ffe78b69eaacf842ad
Tilo Kowalski <privat@tilokowalski.de>
2023-02-04 20:54:29 +0100

    tinymce toolbar

--- src/ViewComponent/FormElement/TinyMCE.php
+++ src/ViewComponent/FormElement/TinyMCE.php
@@ -6,8 +6,9 @@ use Delight\ViewComponent\FormElement\TextArea;
 
 class TinyMCE extends TextArea {
 
-    const DEFAULT_PLUGINS = 'fullscreen table charmap lists wordcount';
-    const DEFAULT_TOOLBAR = 'undo redo | formatselect | bold italic underline | fontsize | alignleft aligncenter alignright alignjustify | numlist bullist | outdent indent | fullscreen removeformat';
+    const DEFAULT_PLUGINS = 'fullscreen table charmap lists wordcount paste autosave';
+    const DEFAULT_TOOLBAR_1 = 'formatselect | bold italic underline | fontsize | alignleft aligncenter alignright alignjustify | numlist bullist | outdent indent';
+    const DEFAULT_TOOLBAR_2 = 'undo redo | preview | fullscreen removeformat | restoredraft';
 
     private ?string $plugins = null;
     private ?string $toolbar = null;
@@ -16,8 +17,12 @@ class TinyMCE extends TextArea {
         $this->plugins = $plugins;
     }
 
-    public function set_toolbar(string $toolbar): void {
-        $this->toolbar = $toolbar;
+    public function set_toolbar_1(string $toolbar_1): void {
+        $this->toolbar_1 = $toolbar_1;
+    }
+
+    public function set_toolbar_2(string $toolbar_2): void {
+        $this->toolbar_2 = $toolbar_2;
     }
 
     public function get_plugins(): string {
@@ -25,10 +30,14 @@ class TinyMCE extends TextArea {
         return $this->plugins;
     }
 
-    public function get_toolbar(): string {
-        if ($this->toolbar == null) return self::DEFAULT_TOOLBAR;
-        return $this->toolbar;
+    public function get_toolbar_1(): string {
+        if ($this->toolbar_1 == null) return self::DEFAULT_TOOLBAR_1;
+        return $this->toolbar_1;
     }
 
-
+    public function get_toolbar_2(): string {
+        if ($this->toolbar_2 == null) return self::DEFAULT_TOOLBAR_2;
+        return $this->toolbar_2;
+    }
+    
 }
