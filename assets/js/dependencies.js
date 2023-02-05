48fc6c75b983ede58a81558b4a4fc38359b86fb9
Tilo Kowalski <privat@tilokowalski.de>
2023-02-05 09:19:42 +0100

    dependencies

--- assets/js/dependencies.js
+++ assets/js/dependencies.js
@@ -4,16 +4,7 @@ window.onload = function() {
     if (typeof window.jQuery == 'undefined') announceProblem('jquery');
     if (typeof $.cookie == 'undefined') announceProblem('jquery.cookie');
 
-    var span = document.createElement('span');
-
-    span.className = 'fa';
-    span.style.display = 'none';
-    document.body.insertBefore(span, document.body.firstChild);
-
-    if ((css(span, 'font-family')) !== 'Font Awesome 6 Free') {
-        announceProblem('@fortawesome/fontawesome-free');
-    }
-    document.body.removeChild(span);
+    // Font Awesome prüfen
 
     if ($('div.delight-vc.tinymce-editor').length) {
         if (typeof tinymce == 'undefined') announceProblem('tinymce (maybe also tinymce-i18n)');
@@ -24,7 +15,3 @@ window.onload = function() {
 function announceProblem(plugin) {
     alert("delight dependency is missing: " + plugin + "\neither not included via npm or script tag with src missing / incorrect");
 }
-
-function css(element, property) {
-    return window.getComputedStyle(element, null).getPropertyValue(property);
-  }
